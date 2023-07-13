68a76ce65bb87983bcde1b99a1905c781cdf1d08
Arash <53490252+ThePythonist@users.noreply.github.com>
2023-07-13 13:14:37 +0330

    Add files via upload

---
+++ 53.py
@@ -0,0 +1,21 @@
+numbers = []
+
+while True:
+    entry = input("Type something : ")
+    if entry == "exit":
+        break
+    else:
+        try:
+            entry = float(entry)
+
+            if str(entry)[-2:] == ".0":
+                numbers.append(int(entry))
+            else:
+                numbers.append(entry)
+
+        except ValueError:
+            # print("Not a number")
+            pass
+
+print(numbers)
+print(max(numbers))
